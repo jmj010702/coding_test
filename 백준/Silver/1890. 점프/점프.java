9cc37a5bfc4f68981d049d2df20f658ea387c44e
jmj010702 <160628908+jmj010702@users.noreply.github.com>
2026-01-12 11:40:43 +0900

    [Silver I] Title: 점프, Time: 116 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] maze = new int[n][n];
+        long[][] dp = new long[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                maze[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dp[0][0] = 1;
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int value = maze[i][j];
+                if(n-1 == i && n-1 ==j) {
+                    continue;
+                }  if (i + value < n ) {
+                    dp[i+value][j] += dp[i][j];
+                }
+                if (j+value <n) {
+                    dp[i][j+value] += dp[i][j];
+                }
+
+            }
+        }
+
+        System.out.println(dp[n - 1][n - 1]);
+    }
+}
